b5c3e6a3c71e1da0788281017b40bff43d69cdb9
Srirangarajan™ <106532953+Eloquencere@users.noreply.github.com>
2023-05-06 08:28:45 +0530

    Update ClockDivider.v

--- Zynq-700-Docs/ClockDivider.v
+++ Zynq-700-Docs/ClockDivider.v
@@ -1,11 +1,11 @@
 reg count = 0;
 reg DividedClk = 0;
 always @(posedge clk) 
-begin
+begin: ClockDivider
     count <= count + 1;
     if(count[24:0])
     begin
         DividedClk <= ~DividedClk;
         count <= 0;
     end
-end
+end: ClockDivider
