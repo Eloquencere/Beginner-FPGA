8eb52e68f43f349e49beed84c637d2efea83cee2
Srirangarajan™ <106532953+Eloquencere@users.noreply.github.com>
2023-05-06 08:28:11 +0530

    Add files via upload

---
+++ Zynq-700-Docs/ClockDivider.v
@@ -0,0 +1,11 @@
+reg count = 0;
+reg DividedClk = 0;
+always @(posedge clk) 
+begin
+    count <= count + 1;
+    if(count[24:0])
+    begin
+        DividedClk <= ~DividedClk;
+        count <= 0;
+    end
+end
